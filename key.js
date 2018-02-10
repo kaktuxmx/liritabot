6dda2fd074a0eaafc63fcf90354f17367eb9c5ee
kaktuxmx <kaktuxsql@gmail.com>
2018-02-10 09:51:33 -0700

    Almost done, need to troubleshoot and to finish loops

--- key.js
+++ key.js
@@ -1,16 +1,19 @@
-
-
 console.log('this is loaded');
 
-exports.twitter = {
-    consumer_key: process.env.B4iNKekW8TnJtnWngAUGCrehF,
-    consumer_secret: process.env.BWa3aGoXD94bFIfOB6Vq1cX5f7ZlMjcJyJdUsr8Kesuxk4Whem,
-    access_token_key: process.env.950975779805921280-dEs3zCcNnnfqK6f4OYmxpg6ktLjudS3,
-    access_token_secret: process.env.KxTQjRxplGT5VPmbvfN5uOdtZVSi7RY4ZloJOza3NnXy6
+
+
+exports.spotifyKeys = {
+    id : process.env.SPOTIFY_ID,
+    secret : process.env.SPOTIFY_SECRET,
 };
 
-exports.spotify = {
-    id: process.env.f4167b8eed5e454198885bc0b3fe456c,
-    secret: process.env.cec10d653a2c4fe59140139bb65fadc3
+exports.twitterKeys = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
+exports.omdbkey = {
+    theapi: process.env.THEAPIKEY  
+};
--- liri.js
+++ liri.js
@@ -1,2 +1,111 @@
 require('dotenv').config()
 
+const keys = require('./key.js');
+const omdb = require('omdb');
+const Spotify = require('node-spotify-api');
+const Twitter = require('twitter');
+const request = require('request');
+
+// Instructions for User
+
+console.log('THIS IS THE FREAKING LIRI!!!');
+console.log('Follow this instructions so you know the amazing things LIRI does ');
+console.log('1. You can type "node liri.js my-tweets" this will help you see tweets from kaktuxdev ');
+console.log('2. You can type "node liri.js spotify-this-song" and the name of a song-like-this');
+console.log('3. You can type "node liri.js movie-this" and the name of a movie-like-this');
+console.log('4. You can type "node liri.js "do-what-it-says" and LIRI will take the stuff from random.txt and do it!');
+
+//Global variables for the different funtions and the switch statement that will be soon.
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
+
+
+//This is the Switch statement, that will select 
+
+// do-what-its-says
+
+var wantitaway = function() {
+                var songsearch = function() {
+
+                    arg2 = "I Want it That Way";
+                    songsearch();
+
+                };
+
+};
+
+
+// Do Spotify Function
+var songsearch = function() {
+    var spotify = new Spotify(keys.spotifyKeys);
+
+    spotify.search({type: 'track', query: arg2 }, function(err, data) { 
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+        
+    console.log('The song name is: ' + data.tracks.items[0].album.name);
+    console.log('The artist/group is: ' + data.tracks.items[0].album.artists[0].name); 
+        letsdoit();
+    });
+};
+
+//Do Twitter stuff.
+ 
+var mytweets = function() {
+
+    var client = new Twitter(keys.twitterKeys);
+    var params = { screen_name: 'kaktuxdev'};
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (i=0; i<tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log(tweets[i].text);
+            };
+        }
+        letsdoit();
+    });
+};
+
+
+var showmovie = function () {
+
+
+
+    request('http://www.omdbapi.com/?t=' + arg2 +'&apikey='+keys.omdbkey.theapi, function (error, response, body) {
+            body = JSON.parse (body);
+            console.log('The Movie is:', body.Title);
+            console.log('The Year was: ' + body.Year);
+
+    });
+
+
+}
+
+
+var letsdoit = function(anotherarg1) { // if 
+    switch (anotherarg1){
+        case 'my-tweets':
+            mytweets();
+            break;
+        case 'spotify-this-song':
+            songsearch();
+            break;
+        case 'movie-this':
+            showmovie();
+        case 'do-what-it-says':
+            wantitaway();
+        break;
+        default:
+            console.log("What the hell are you doing??? Pick the correct one");
+        };
+
+};
+
+if (arg1) {
+    letsdoit(arg1);
+} else {
+    console.log("Need to look the instructins")   
+}
+
+
